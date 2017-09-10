4d7a036b553bd96ac29e4e992a17ac035768f184
Anakorn Kyavatanakij <anakornk@gmail.com>
2017-09-10 16:42:36 +0800

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "bots#index"
+  get '/webhook', to: "bots#validate"
+  post '/webhook', to: "bots#webhook"
 end
